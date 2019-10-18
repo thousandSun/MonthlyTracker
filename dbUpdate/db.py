9f4c19930423882344bf65b6c599a249eb9d34e2
mugiwaraace <filibertoerios@gmail.com>
2019-10-18 16:26:58 -0500

    added method to retrieve data from db table

--- dbUpdate/db.py
+++ dbUpdate/db.py
@@ -5,20 +5,35 @@ def create_table():
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS expenses(category text, name text primary key, total real, payment real, remaining real, paid real)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS expenses(name text primary key, total real, payment real, remaining real, paid real)")
 
     connection.commit()
     connection.close()
 
 
-def add_expense(category, name, total, payment):
-    conection = sqlite3.connect('data.db')
+def add_expense(name, total, payment):
+    connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
 
-    #cursor.execute(f'INSERT INTO expenses VALUES("{category}", "{name}", {total}, {payment}, {total}, 0.0)')
+    cursor.execute('INSERT INTO expenses VALUES(?, ?, ?, ?, ?)', (name, total, payment, total, 0))
+
 
     connection.commit()
+    connection.close()
+
+
+def get_values():
+    connection = sqlite3.connect("data.db")
+    connection.text_factory = str
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM expenses')
+    expenses = [{'name': row[0], 'total': row[1], 'payment': row[2], 'remaining': row[3], 'paid': row[4]} for row in cursor.fetchall()]  # gives a list of tuples
+    print(expenses)
+
     connection.close()
 
 
 create_table()
+#add_expense('house', 200000, 1145.6)
+get_values()
