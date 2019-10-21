c91fb567ea23604db45b1d58cebd56c53d32975c
mugiwaraace <filibertoerios@gmail.com>
2019-10-20 23:35:41 -0500

    finished most of the db backend interface, finished ui, in process of adding method to increse remaining balance

--- dbUpdate/db.py
+++ dbUpdate/db.py
@@ -5,35 +5,190 @@ def create_table():
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS expenses(name text primary key, total real, payment real, remaining real, paid real)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS expenses(name text primary key, total real, payment real, "
+                   "remaining real, paid real, complete BIT)")
 
     connection.commit()
     connection.close()
 
 
-def add_expense(name, total, payment):
+def show_expenses():
+    expenses = _get_bills()
+
+    for bill in expenses:
+        bill_name = bill['name']
+        if not bool(bill['complete']):
+            bill_total = bill['total']
+            bill_payment = bill['payment']
+            bill_remaining = bill['remaining']
+            bill_paid = bill['paid']
+
+            bill_string = f'| {bill_name.title()}: Total: {bill_total:,.2f} Payment: {bill_payment:,.2f} ' \
+                          f'Remaining: {bill_remaining:,.2f} Paid to Date: {bill_paid:,.2f} |'
+            print("-" * len(bill_string))
+            print(bill_string)
+            print("-" * len(bill_string))
+        else:
+            bill_string = f'| {bill_name.title()}: PAID IN FULL |'
+            print('-' * len(bill_string))
+            print(bill_string)
+            print('-' * len(bill_string))
+
+
+def add_bill(name, payment, total):
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO expenses VALUES(?, ?, ?, ?, ?)', (name, total, payment, total, 0))
-
+    cursor.execute('INSERT INTO expenses VALUES(?, ?, ?, ?, ?, ?)', (name, total, payment, total, 0, 0))
 
     connection.commit()
     connection.close()
 
 
-def get_values():
+def make_payment(name, amount):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute('SELECT remaining FROM expenses WHERE name=?', (name,))
+        expense_remaining = cursor.fetchone()[0]
+        cursor.execute('SELECT paid FROM expenses WHERE name=?', (name,))
+        expense_paid = cursor.fetchone()[0]
+    except TypeError:
+        print('!! Invalid Query !!')
+        connection.close()
+        return
+
+    expense_remaining -= amount
+    if expense_remaining <= 0:
+        cursor.execute('UPDATE expenses SET complete=? WHERE name=?', (1, name))
+        print('aaaa')
+    else:
+        expense_paid += amount
+        cursor.execute('UPDATE expenses SET remaining=? WHERE name=?', (expense_remaining, name))
+        cursor.execute('UPDATE expenses SET paid=? WHERE name=?', (expense_paid, name))
+        cursor.execute('UPDATE expenses SET payment=? WHERE name=?', (amount, name))
+
+    connection.commit()
+    connection.close()
+
+
+def quick_pay(name):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute('SELECT remaining FROM expenses WHERE name=?', (name,))  # finish quick pay method
+        expense_remaining = cursor.fetchone()[0]
+        cursor.execute('SELECT payment FROM expenses WHERE name=?', (name,))
+        expense_payment = cursor.fetchone()[0]
+        cursor.execute('SELECT paid FROM expenses WHERE name=?', (name,))
+        expense_paid = cursor.fetchone()[0]
+    except TypeError:
+        print('!! Invalid Query !!')
+        connection.close()
+        return
+
+    expense_remaining -= expense_payment
+    if expense_remaining <= 0:
+        cursor.execute('UPDATE expenses SET complete=? WHERE name=?', (1, name))
+    else:
+        expense_paid += expense_payment
+        cursor.execute('UPDATE expenses SET remaining=? WHERE name=?', (expense_remaining, name))
+        cursor.execute('UPDATE expenses SET paid=? WHERE name=?', (expense_paid, name))
+
+    connection.commit()
+    connection.close()
+
+
+def remove(name):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('DELETE FROM expenses WHERE name=?', (name,))
+
+    connection.commit()
+    connection.close()
+
+
+def update_bill(name):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    update_string = """What property do you want to update
+    --> name
+    --> total
+    --> payment
+Selection: """
+    expense = _get_bill(name)
+    if expense is not None:
+        updated_field = input(update_string).lower()
+        if updated_field == 'name':
+            new_name = input('New name: ').lower()
+            cursor.execute('UPDATE expenses SET name=? WHERE name=?', (new_name, name))
+        elif updated_field == 'total':
+            new_total = _to_float(input('New total: '))
+            if new_total is not None:
+                cursor.execute('UPDATE expenses SET total=? WHERE name=?', (new_total, name))
+            else:
+                pass
+        elif updated_field == 'payment':
+            new_payment = _to_float(input('New payment: '))
+            if new_payment is not None:
+                cursor.execute('UPDATE expenses SET payment=? WHERE name=?', (new_payment, name))
+            else:
+                pass
+        else:
+            print('!! Invalid Selection !!')
+        connection.commit()
+    else:
+        print("!! Invalid Query !!")
+
+    connection.close()
+
+
+def add_expense(name, amount):  # finish making method to add expenditure e.g. total = 500, spent = 50, remaining = 550
+    pass
+
+
+def _get_bills():
     connection = sqlite3.connect("data.db")
     connection.text_factory = str
     cursor = connection.cursor()
 
     cursor.execute('SELECT * FROM expenses')
-    expenses = [{'name': row[0], 'total': row[1], 'payment': row[2], 'remaining': row[3], 'paid': row[4]} for row in cursor.fetchall()]  # gives a list of tuples
-    print(expenses)
+    expenses = [{'name': row[0], 'total': row[1], 'payment': row[2],
+                 'remaining': row[3], 'paid': row[4], 'complete': row[5]} for row in cursor.fetchall()]
 
     connection.close()
+    return expenses
 
 
-create_table()
-#add_expense('house', 200000, 1145.6)
-get_values()
+def _get_bill(name):
+    connection = sqlite3.connect("data.db")
+    connection.text_factory = str
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute('SELECT * FROM expenses WHERE name=?', (name,))
+        expense = cursor.fetchone()
+        expense = {'name': expense[0], 'total': expense[1], 'payment': expense[2],
+                   'remaining': expense[3], 'paid': expense[4], 'complete': expense[5]}
+    except TypeError:
+        connection.close()
+        return None
+
+    connection.close()
+    return expense
+
+
+def _to_float(variable):
+    variable = variable.split(',')
+    variable = ''.join(variable)
+
+    try:
+        variable = float(variable)
+    except ValueError:
+        return None
+
+    return variable
---
+++ dbUpdate/userinterface.py
@@ -0,0 +1,135 @@
+import dbUpdate.db as db
+
+WELCOME_MESSAGE = """Welcome to MonthlyTracker v0.4!"""
+
+USER_PROMPT = """You have the following options available:
+--> 1 - add expense
+--> 2 - make payment
+--> 3 - remove expense
+--> 4 - quick pay
+--> 5 - update expense
+--> 6 - help
+--> 9 - exit
+--> 0 - show credits
+
+Make a selection: """
+
+HELP_MESSAGE = """Welcome to Help
+
+Option 1 - add expense
+    this option lets you add a monthly expenditure to your list of
+    expenses
+
+Option 2 - make payment
+    this option allows you to enter a custom payment amount that
+    will be saved to use in `quick pay` functionality
+
+option 3 - remove expense
+    this option does what it says, it removes a specific expense
+
+Option 4 - quick pay
+    this option allows you to use last months payment amount as
+    this months payment amount, you don't enter a payment amount
+
+Option 5 - update expense
+    allows you to change the name, total, and payment amount of an expense
+
+Option 6 - help
+    displays this somewhat useful help message
+
+Option 9 - exit program
+    closes the program
+
+Option 0 - show credits
+    shows the author and the contributors to this project
+"""
+
+CREDITS_MESSAGE = """
+!!!!!!!!!!!!!!!!!!!!!!!!!
+Written by Filiberto Rios
+!!!!!!!!!!!!!!!!!!!!!!!!!
+*************
+Contributors:
+*************
+"""
+
+
+def menu():
+    db.create_table()
+    print(WELCOME_MESSAGE)
+    user_choice = ''
+    while user_choice != 9:
+        db.show_expenses()
+        try:
+            user_choice = int(input(USER_PROMPT))
+        except ValueError:
+            print('!! Invalid Selection !!')
+            continue
+
+        if user_choice == 1:
+            add()
+        elif user_choice == 2:
+            make_payment()
+        elif user_choice == 3:
+            remove()
+        elif user_choice == 4:
+            quick_pay()
+        elif user_choice == 5:
+            update()
+        elif user_choice == 6:
+            print(HELP_MESSAGE)
+        elif user_choice == 9:
+            break
+        elif user_choice == 0:
+            print(CREDITS_MESSAGE)
+
+
+def add():
+    name = input("Bill name: ").lower()
+    total = _to_float(input("Total : $"))
+    payment = _to_float(input("Payment: $"))
+
+    if total is not None and payment is not None:
+        db.add_bill(name, payment, total)
+    else:
+        print(f'{"!! Invalid total amount" if total is None else "!! Invalid payment amount"}, try again !!')
+
+
+def make_payment():
+    name = input("Bill name: ").lower()
+    payment = _to_float(input("Payment: $"))
+
+    if payment is not None:
+        db.make_payment(name, payment)
+    else:
+        print('!! Invalid payment amount !!')
+
+
+def remove():
+    name = input("Bill to remove: ")
+    db.remove(name)
+
+
+def quick_pay():
+    name = input("Bill name: ")
+    db.quick_pay(name)
+
+
+def update():
+    name = input("Bill name: ")
+    db.update_bill(name)
+
+
+def _to_float(variable):
+    variable = variable.split(',')
+    variable = ''.join(variable)
+
+    try:
+        variable = float(variable)
+    except ValueError:
+        return None
+
+    return variable
+
+
+menu()
